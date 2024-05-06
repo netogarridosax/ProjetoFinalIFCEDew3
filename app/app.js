07278186c408671ae027a712fb44f0610f5baa71
Garrido Neto <134981206+netogarridosax@users.noreply.github.com>
2024-05-05 21:35:53 -0300

    correcao

--- app/app.js
+++ app/app.js
@@ -11,16 +11,17 @@ const multer = require('multer');
 const Servico = require('./models/Servico');
 const router = require('./routes/index');
 
-require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
+require('dotenv').config();
 
-// Defina as chaves VAPID - você pode gerar essas chaves usando o web-push
 webpush.setVapidDetails(
     'mailto:seuemail@example.com',
     process.env.VAPID_PUBLIC_KEY,
     process.env.VAPID_PRIVATE_KEY
 );
 
-mongoose.connect(process.env.MONGODB_URI)
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/seu_banco_de_dados';
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Conexão com o MongoDB estabelecida com sucesso');
     })
@@ -57,11 +58,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 let subscriptions = [];
 
-// Rota para salvar as subscrições
 app.post('/subscribe', (req, res) => {
     subscription = req.body;
     subscriptions.push(subscription);
-    console.log({ subscriptions });
     res.status(201).json({});
 });
 
@@ -69,22 +68,18 @@ app.get('/push', (req, res) => {
     res.render('push');
 });
 
-// Rota para enviar notificações
 app.get('/notificar', (req, res) => {
     const payload = JSON.stringify({ title: req.query.msg });
-    console.log('notificando', subscriptions);
     for (let subscription of subscriptions) {
         webpush.sendNotification(subscription, payload)
             .catch(error => console.error('Erro ao notificar:', error));
-        console.log('notificando', subscription);
     }
     res.send('ok');
 });
 
-// Configuração do multer para upload de imagens
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'public/_img'); // Salva as imagens na pasta 'public/_img'
+        cb(null, 'public/_img');
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
@@ -93,15 +88,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// Registrar o service worker
 app.get('/worker.js', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'worker.js'));
 });
 
 app.get('/', async (req, res) => {
     try {
-        const servicos = await Servico.find(); // Consulta todos os servicos no banco de dados
-        res.render('index', { servicos, carrinho: req.session.carrinho || [] }); // Passa a lista de servicos e o carrinho para o template
+        const servicos = await Servico.find();
+        res.render('index', { servicos, carrinho: req.session.carrinho || [] });
     } catch (error) {
         console.error('Erro ao carregar servicos:', error);
         res.status(500).send('Erro ao carregar servicos');
@@ -110,19 +104,17 @@ app.get('/', async (req, res) => {
 
 const carrinho = [];
 
-// Rota para adicionar servicos ao carrinho
 app.post('/adicionar-ao-carrinho/:id', async (req, res) => {
     const servicoId = req.params.id;
     const servico = await Servico.findById(servicoId);
     if (servico) {
         carrinho.push(servico);
-        res.redirect('/'); // Redireciona para a página inicial após adicionar ao carrinho
+        res.redirect('/');
     } else {
         res.status(404).send('Servico não encontrado');
     }
 });
 
-// Middleware para inicializar o carrinho na sessão
 app.use((req, res, next) => {
     if (!req.session.carrinho) {
         req.session.carrinho = [];
@@ -130,7 +122,6 @@ app.use((req, res, next) => {
     next();
 });
 
-// Rota para exibir o formulário de criação de servicos
 app.get('/servicos/criar', (req, res) => {
     res.render('criar_servico', { 
         success: req.flash('success'),
@@ -138,12 +129,10 @@ app.get('/servicos/criar', (req, res) => {
     });
 });
 
-// Rota para processar o formulário de criação de servicos
 app.post('/servicos', upload.single('imagem'), async (req, res) => {
     try {
-        let imagem = req.file ? req.file.filename : null; // Salva o nome do arquivo no campo imagem
+        let imagem = req.file ? req.file.filename : null;
         const novoServico = await Servico.create({...req.body, imagem: imagem});
-        console.log('Novo servico criado:', novoServico);
         req.flash('success', 'Servico cadastrado com sucesso');
         res.redirect('/servicos/criar');
     } catch (error) {
@@ -153,7 +142,6 @@ app.post('/servicos', upload.single('imagem'), async (req, res) => {
     }
 });
 
-// Rota para exibir o formulário de edição de servicos
 app.get('/servicos/editar/:id', async (req, res) => {
     try {
         const servico = await Servico.findById(req.params.id);
@@ -161,7 +149,7 @@ app.get('/servicos/editar/:id', async (req, res) => {
             req.flash('error', 'Servico não encontrado');
             return res.redirect('/servicos');
         }
-        res.render('editar_servico', { servico, success: req.flash('success'), error: req.flash('error') }); // Passando a variável error
+        res.render('editar_servico', { servico, success: req.flash('success'), error: req.flash('error') });
     } catch (error) {
         console.error('Erro ao buscar servico:', error);
         req.flash('error', 'Erro ao buscar servico');
@@ -177,18 +165,14 @@ app.put('/servicos/:id', async (req, res) => {
             return res.redirect('/servicos');
         }
 
-        // Atualize os campos do servico com os valores do corpo da requisição
         servico.nome = req.body.nome;
         servico.categoria = req.body.categoria;
         servico.preco = req.body.preco;
         servico.tamanho = req.body.tamanho;
         servico.descricao = req.body.descricao;
 
-        // Salve as alterações no banco de dados
         await servico.save();
         
-        console.log('Servico atualizado:', servico);
-
         req.flash('success', 'Servico atualizado com sucesso');
         res.redirect('/servicos');
     } catch (error) {
